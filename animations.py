0f8e4f01e10d05add7a9752827cdfdb84bcbacf8
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-04 16:33:49 -0700

    uploaded animations.py

---
+++ animations.py
@@ -0,0 +1,129 @@
+# By Jean-Jacques F. Reibel
+# Cyclops from X-Men
+# I will not be held responsible for:
+# any shenanigans
+# ಠ_ಠ
+# ¯¯\_(ツ)_/¯¯
+
+import os
+
+os.system("printf '\e[0;31;1;1m\n   Flexing Skullhead\n'")
+os.system("printf '\e[0;35;1;1m\n         ( 💀 )'")
+os.system("printf '\e[0;31;1;1m\n          (__)'")
+os.system("printf '\e[0;33;1;1m\n        __'")
+os.system("printf '\e[0;34;1;1m(  )'")
+os.system("printf '\e[0;33;1;1m__'")
+
+
+os.system("printf '\e[0;31;1;1m\n\n   Flexing Chthulhu\n'")
+os.system("printf '\e[0;39;1;1m\n         ( 🐙 )'")
+os.system("printf '\e[0;34;1;1m\n          (__)'")
+os.system("printf '\e[0;33;1;1m\n        __'")
+os.system("printf '\e[0;34;1;1m(  )'")
+os.system("printf '\e[0;33;1;1m__'")
+
+
+os.system("printf '\e[0;39;1;1m\n\n        Octopy\n'")
+os.system("printf '\e[0;39;1;1m          __\n'")
+os.system("printf '\e[0;39;1;1m      ___('")
+os.system("printf '\e[0;31;1;1mಠಠ'")
+os.system("printf '\e[0;39;1;1m)___\n'")
+os.system("printf '\e[0;39;1;1m     ( ( (||) )  '")
+os.system("printf '\e[0;39;1;5m)\n'")
+os.system("printf '\e[0;39;1;1m     ) ) )||( (  '")
+os.system("printf '\e[0;39;1;5m(__\n'")
+os.system("printf '\e[0;39;1;1m    ( ( ( || ) )    '")
+os.system("printf '\e[0;39;1;5m)\n'")
+os.system("printf '\e[0;39;1;1m    ) ) ) || ( (   '")
+os.system("printf '\e[0;39;1;5m(__\n'")
+os.system("printf '\e[0;39;1;1m   ( ( (  ||  ) )     '")
+os.system("printf '\e[0;39;1;5m)\n\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n        Idea\n'")
+os.system("printf '\e[0;39;1;5m\n         💡\n'")
+os.system("printf '\e[0;39;1;1m         😲\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n   Fire Breathing Dragon\n'")
+os.system("printf '\e[0;39;1;5m\n         🔥'")
+os.system("printf '\e[0;39;1;1m🐉\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n      Hadouken\n'")
+os.system("printf '\e[0;39;1;5m\n         🔥\n'")
+os.system("printf '\e[0;39;1;1m         👐\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n       Spider\n'")
+os.system("printf '\e[0;39;1;5m\n         🕸️\n'")
+os.system("printf '\e[0;39;1;1m         🕷️\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n    Thunder Cloud\n'")
+os.system("printf '\e[0;39;1;1m\n         ☁️\n'")
+os.system("printf '\e[0;39;1;5m         ⚡\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n        Zeus\n'")
+os.system("printf '\e[0;39;1;5m\n         ⚡\n'")
+os.system("printf '\e[0;39;1;1m         🔱\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n        Devil\n'")
+os.system("printf '\e[0;39;1;5m\n         🔥\n'")
+os.system("printf '\e[0;39;1;1m         🔱\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n        Boom\n'")
+os.system("printf '\e[0;39;1;1m\n       🤜'")
+os.system("printf '\e[0;39;1;5m💥'")
+os.system("printf '\e[0;39;1;1m🤛\n\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n      Divinity\n'")
+os.system("printf '\e[0;39;1;5m\n         ✨\n'")
+os.system("printf '\e[0;39;1;5m       ✨✨✨'")
+os.system("printf '\e[0;39;1;5m\n     ✨✨'")
+os.system("printf '\e[0;39;1;1m🔮'")
+os.system("printf '\e[0;39;1;5m✨✨\n\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n             Wizard\n\n'")
+os.system("printf '\e[0;39;1;5m     🔥🔥🔥🔥      🔥🔥🔥🔥\n'")
+os.system("printf '\e[0;39;1;5m     🔥🔥🔥🔥      🔥🔥🔥🔥\n'")
+os.system("printf '\e[0;39;1;5m     🔥🔥🔥🔥      🔥🔥🔥🔥\n'")
+os.system("printf '\e[0;39;1;5m     🔥🔥🔥🔥  '")
+os.system("printf '\e[0;39;1;1m🧙‍♂️'")
+os.system("printf '\e[0;39;1;5m  🔥🔥🔥🔥\n\n'")
+
+
+os.system("printf '\e[0;39;1;1m\n\n       Boo!\n'")
+os.system("printf '\e[0;39;1;1m\n     😱'")
+os.system("printf '\e[0;39;1;5m    👻\n\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n        Love\n'")
+os.system("printf '\e[0;39;1;5m\n     ❤️       💗'")
+os.system("printf '\e[0;39;1;1m\n     🥰  🌹  👸\n\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n       Candle\n'")
+os.system("printf '\e[0;39;1;5m\n         🔥\n'")
+os.system("printf '\e[0;39;1;1m         🕯️\n\n'")
+
+
+os.system("printf '\e[0;39;1;1m\n\n        Cake\n'")
+os.system("printf '\e[0;39;1;5m\n         🔥\n'")
+os.system("printf '\e[0;39;1;1m         🎂\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n        Witch\n'")
+os.system("printf '\e[0;39;1;5m\n          ✨\n'")
+os.system("printf '\e[0;39;1;1m   🎃   🧙‍♀️🔮   🎃\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n        Medium\n'")
+os.system("printf '\e[0;39;1;5m\n       ✨👻✨\n'")
+os.system("printf '\e[0;39;1;1m       🧙‍♀️🔮    \n\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n      Explosion\n'")
+os.system("printf '\e[0;39;1;5m\n         💥\n'")
+os.system("printf '\e[0;39;1;1m         💣\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n     Explosion 2\n'")
+os.system("printf '\e[0;39;1;5m\n         💥\n'")
+os.system("printf '\e[0;39;1;1m         🧨\n'")
+
+os.system("printf '\e[0;39;1;1m\n\n        Good\n'")
+os.system("printf '\e[0;39;1;5m\n         🔥\n'")
+os.system("printf '\e[0;39;1;1m         👍\n\n'")
+
+
+
